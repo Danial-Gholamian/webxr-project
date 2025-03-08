9284c18f0a4d9681c8c7fdc2809700fb92dfbda6
Danial Gholamina <dg222mm@student.lnu.se>
2025-03-08 13:31:59 +0100

    test19

--- cubes.js
+++ cubes.js
@@ -24,7 +24,7 @@ export const labels = [];
 const cubeGeometry = new THREE.BoxGeometry(0.3, 0.3, 0.3);
 const colors = [0xff0000, 0x00ff00, 0x0000ff, 0xffff00, 0xff00ff];
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 100; i++) {
     const cubeMaterial = new THREE.MeshStandardMaterial({ color: colors[i % colors.length] });
     const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
     cube.userData.index = i; // Store index inside cube metadata
